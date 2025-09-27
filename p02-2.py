162506ac62808fe90d45d9517670bbffbb3df43d
Jakab Tamás <micikeh.666@gmail.com>
2025-09-27 11:12:19 +0200

    def

--- p02-2.py
+++ p02-2.py
@@ -1,7 +1,37 @@
-def teglalp(a, b):
+'''def teglalp():
+    a = 5
+    b = 4
+    kerulet = 2 * a + 2 * b
+    return kerulet
+
+a = 4
+b = 3
+print("A kerület ", teglalp())
+
+def teglalap_ker(a, b):
     #a = 5
     #b = 4
     kerulet = 2 * a + 2 * b
     return kerulet
 
-teglalp(3,5)
+a = 4
+b = 3
+print("A kerület ", teglalap_ker(a, b))
+
+def teglalap_ter(x, y):
+    terulet = y * x
+    return terulet
+
+x = 4
+y = 3
+print("A terület ", teglalap_ter(x, y))'''
+
+def teglalap_ker_ter(x, y):
+    kerulet = 2 * x + 2 * y
+    terulet = y * x
+    return kerulet, terulet
+
+x = 4
+y = 3
+print("A kerulet ", teglalap_ker_ter(x, y)[0])
+print("A terület ", teglalap_ker_ter(x, y)[1])
