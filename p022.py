27528193708ca00f56a3c22cbf24541ca607095d
Jakab Tamás <micikeh.666@gmail.com>
2025-09-27 11:22:43 +0200

    def

--- p022.py
+++ p022.py
@@ -24,7 +24,7 @@ def teglalap_ter(x, y):
 
 x = 4
 y = 3
-print("A terület ", teglalap_ter(x, y))'''
+print("A terület ", teglalap_ter(x, y))
 
 def teglalap_ker_ter(x, y):
     kerulet = 2 * x + 2 * y
@@ -34,4 +34,19 @@ def teglalap_ker_ter(x, y):
 x = 4
 y = 3
 print("A kerulet ", teglalap_ker_ter(x, y)[0])
-print("A terület ", teglalap_ker_ter(x, y)[1])
+print("A terület ", teglalap_ker_ter(x, y)[1])'''
+
+def negyszog(x, y):
+    kerulet = 2 * x + 2 * y
+    terulet = y * x
+    if x == y:
+        alakzat = "negyzet"
+    else:
+        alakzat = "teglalap"
+    return kerulet, terulet, alakzat
+
+a = 4
+b = 3
+eredmeny = negyszog(a, b)
+print(f"A {eredmeny[2]} kerulet ", eredmeny[0])
+print(f"A {eredmeny[2]} terület ", eredmeny[1])
