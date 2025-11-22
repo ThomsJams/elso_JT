ee9f22d04b1bd8e65e32b1cffc4b578d7e89547a
Jakab Tamás <micikeh.666@gmail.com>
2025-11-22 11:27:40 +0100

    p3

--- p3_back.py
+++ p3_back.py
@@ -1,3 +1,9 @@
-import flask
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
